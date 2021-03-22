2de4b610c9a6257d1c90e74c142bc9214239a715
unknown <wendellmatheus77@gmail.com>
2021-03-22 18:12:54 -0300

    create restaurants-service

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,3 +1,4 @@
+import { RestaurantsService } from './restaurant.service';
 import { Restaurant } from './restaurant/restaurant.model';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,27 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[] = [
-    {
-    id: "bread-bakery",
-    name: "Bread & Bakery",
-    category: "Bakery",
-    deliveryEstimate: "25m",
-    rating: 4.9,
-    imagePath: "assets/img/restaurants/breadbakery.png",
-  },
-  {
-    id: "burger-house",
-    name: "Burger House",
-    category: "Hamburgers",
-    deliveryEstimate: "100m",
-    rating: 3.5,
-    imagePath: "assets/img/restaurants/burgerhouse.png",
-  },]
+  restaurants: Restaurant[]
 
-  constructor() { }
+  constructor(private RestaurantsService: RestaurantsService) { }
 
   ngOnInit() {
+    this.restaurants = this.RestaurantsService.restaurants()
   }
 
 }

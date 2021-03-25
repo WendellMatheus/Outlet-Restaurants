c62a2d2d83982b4e4ed130abd0a98d3804aaa002
unknown <wendellmatheus77@gmail.com>
2021-03-25 15:41:47 -0300

    create validations forms

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ContentChild, NgModule, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+
 
 @Component({
-  selector: 'mt-input',
+  selector: 'mt-input-container',
   templateUrl: './input.component.html',
 })
 export class InputComponent implements OnInit {
 
+  @Input() label: string
+  @Input() errorMessage: string
+
+  input: any
+
+  @ContentChild(NgModule) model: NgModule
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    this.input = this.model
+    if(this.input === undefined){
+      throw new Error('Esse componente precisa ser usado com uma diretiva ngModel')
+    }
+  }
+
+  hasSuccess(): boolean {
+    return this.input.valid && (this.input.dirty || this.input.touched)
+  }
+  hasError(): boolean {
+    return this.input.invalid && (this.input.dirty || this.input.touched)
   }
 
 }

344ced0dc55132abd4517ab73aa6d94f06b269f3
unknown <wendellmatheus77@gmail.com>
2021-03-25 11:34:52 -0300

    create reviews

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from "@angular/core"
 import { Http } from "@angular/http"
 
 import { Observable } from "rxjs"
-
-import { Restaurant } from "./restaurant/restaurant.model"
-
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+
+import { Restaurant } from "./restaurant/restaurant.model"
+import { MenuItem } from "app/restaurant-detail/menu-item/menu-item.model"
 
 import { MEAT_API } from "../app.api"
 import { ErrorHandler } from '../app.error-handler';
@@ -29,5 +29,15 @@ export class RestaurantsService{
     .catch(ErrorHandler.handlerError)
   }
 
+  reviewsOfRestaurant(id: string): Observable<any>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handlerError)
+  }
 
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handlerError)
+  }
 }

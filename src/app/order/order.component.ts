233c3e6c61a12e85b64f32a807ac6c0a17f5c55b
unknown <wendellmatheus77@gmail.com>
2021-03-29 19:07:21 -0300

    create component order-summary

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router'
+
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
@@ -19,7 +21,7 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: 'REF'}
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -49,7 +51,7 @@ export class OrderComponent implements OnInit {
       .map((item:CartItem)=> new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
       .subscribe( (orderId: string) => {
-    console.log(`compra conlcuída: ${orderId}`)
+        this.router.navigate(['/order-summary'])
       this.orderService.clear()
     })
     console.log(order)

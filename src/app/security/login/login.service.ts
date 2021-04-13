4c99b06253796a2b7d47f59643f29a63929724fc
unknown <wendellmatheus77@gmail.com>
2021-04-13 13:23:53 -0300

    update login

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
+
 import { User } from './user.model';
 import { MEAT_API } from '../../app.api';
 
@@ -12,8 +14,12 @@ import { MEAT_API } from '../../app.api';
 export class LoginService {
 
   user: User
+  lastUrl: string
 
-  constructor(private http: HttpClient, private router: Router){}
+  constructor(private http: HttpClient, private router: Router){
+    this.router.events.filter(e => e instanceof NavigationEnd)
+                      .subscribe((e: NavigationEnd) => this.lastUrl = e.url)
+  }
 
   isLoggedIn(): boolean {
     return this.user !== undefined
@@ -21,11 +27,15 @@ export class LoginService {
 
   login(email: string, password: string): Observable<User>{
     return this.http.post<User>(`${MEAT_API}/login`,
-          {email: email, password: password})
+                {email: email, password: password})
           .do(user => this.user = user)
   }
 
-  handleLogin(path?: string){
+  logout(){
+    this.user = undefined
+  }
+
+  handleLogin(path: string = this.lastUrl){
     this.router.navigate(['/login', btoa(path)])
   }
 }
